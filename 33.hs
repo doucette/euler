8cbd919da0cf859077a1ea54135133e5295ff8ad
Devin Doucette <devin@doucette.cc>
2011-06-05 20:50:26 -0600

    33

---
+++ 33.hs
@@ -0,0 +1,8 @@
+import Ratio
+main = print . denominator . product $ [ (n % d) | n1 <- [1..9], n2d1 <- [1..9]
+                                       , d2 <- [1..9]
+                                       , let n = n1 * 10 + n2d1
+                                       , let d = n2d1 * 10 + d2
+                                       , n < d
+                                       , n1 % d2 == n % d
+                                       ]
